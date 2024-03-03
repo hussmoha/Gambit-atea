be1cea4a35aa3456a56d9f67d2e9c965094239db
Mohamed Hussein <e2001945@edu.vamk.fi>
2024-03-03 02:21:36 +0200

    Changes to Nextauth

--- src/app/api/auth/[...nextauth].ts
+++ src/app/api/auth/[...nextauth].ts
@@ -1,9 +1,9 @@
-
+import type { NextAuthOptions } from "next-auth";
 import NextAuth from "next-auth";
 import GitHubProvider from "next-auth/providers/github";
 import GoogleProvider from "next-auth/providers/google";
 
-export default NextAuth({
+export const options: NextAuthOptions = {
   providers: [
     GitHubProvider({
       clientId: process.env.GITHUB_CLIENT_ID as string,
@@ -14,4 +14,8 @@ export default NextAuth({
       clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
     }),
   ],
-});
+};
+
+const handler = NextAuth(options);
+
+export { handler as GET, handler as POST };
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,4 @@
-"use client";
+
 
 import { signIn } from "next-auth/react";
 
