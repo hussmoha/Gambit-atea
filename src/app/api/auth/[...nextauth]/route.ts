92c2fa6087e3cda92c6efe861b57f512b62681da
Mohamed Hussein <e2001945@edu.vamk.fi>
2024-03-03 14:19:51 +0200

    Changes to api route

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,20 +1,5 @@
-import type { NextAuthOptions } from "next-auth";
+import { options } from "./options";
 import NextAuth from "next-auth";
-import GitHubProvider from "next-auth/providers/github";
-import GoogleProvider from "next-auth/providers/google";
-
-export const options: NextAuthOptions = {
-  providers: [
-    GitHubProvider({
-      clientId: process.env.GITHUB_CLIENT_ID as string,
-      clientSecret: process.env.GITHUB_CLIENT_SECRET as string,
-    }),
-    GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID as string,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
-    }),
-  ],
-};
 
 const handler = NextAuth(options);
 
